11e40238a5c423227deb58a185c11d56e9e464f6
QuangThang1804 <thangpham1804@gmail.com>
2022-10-04 09:12:47 +0700

    update login

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./svPage.html", "./gvPage.html", "./adminPage"],
+  content: ["./index.html", "./svPage.html", "./gvPage.html", "./adminPage", "login.html"],
   theme: {
     extend: {},
   },
