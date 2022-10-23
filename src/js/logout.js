0f7378ea0abfe9e46e888990d3f53910d2a4d8e5
QuangThang1804 <thangpham1804@gmail.com>
2022-10-23 23:40:31 +0700

    update admin js

--- src/js/logout.js
+++ src/js/logout.js
@@ -1,14 +1,16 @@
-// import logoutUser from "../apiServices/user/logoutUser.js";
+import logoutUser from "../apiServices/user/logoutUser.js";
 
-// const buttonLogout = document.getElementById("buttonLogout");
+const buttonLogout = document.getElementById("buttonLogout");
+console.log(buttonLogout);
 
-// async function handerLogout() {
-//     const res = await logoutUser()
-//     const buttonAvatar = document.getElementById("buttonAvatar");
-//     const buttonRegister = document.getElementById("buttonRegister");
-//     buttonRegister.style.display = "block";
-//     buttonAvatar.style.display = "none";
-//     console.log("res", res)
-// }
+async function handerLogout() {
+    const res = await logoutUser()
+    const buttonAvatar = document.getElementById("buttonAvatar");
+    const buttonRegister = document.getElementById("buttonRegister");
+    buttonRegister.style.display = "block";
+    buttonAvatar.style.display = "none";
+    console.log(buttonAvatar);
+    console.log("res", res)
+}
 
-// buttonLogout.addEventListener('click', handerLogout());
+buttonLogout.addEventListener('click', handerLogout);
